f8df9552632625a28a2cbc1e8b32416e07b9092a
Len <len.pgh@gmail.com>
2023-07-26 15:28:44 -0400

    Update swml-demo.php

--- php/swml-demo.php
+++ php/swml-demo.php
@@ -1,45 +1,108 @@
-#!/usr/bin/env php
 <?php
 
-require_once 'SignalWireML.php';
+require_once 'signalwireml.php';
 
 use SignalWire\ML;
 
-$swml = new SignalWireML(['version' => '1.0.2', 'voice' => 'en-GB-Neural2-F']);
+$swml = new SignalWireML();
+
+$swml->add_ailanguage([
+    'name' => 'English',
+    'code' => 'en-US',
+    'voice' => 'en-US-Neural2-F'
+]);
+
+$swml->add_aiparams(['smsFromNumber' => '+19184588888']);
+$swml->add_aiparams(['languagesEnabled' => 'true']);
+$swml->add_aiparams(['languageMode' => 'normal']);
 
 $swml->set_aiprompt([
     'temperature' => '0.9',
-    'top_p' => '1.0',
-    'text' => 'Your name is Olivia, You are able to lookup weather and time for various locations.  Caller can ask anything.'
+    'top_p' => '0.9',
+    'text' => "You name is Franklin and you are an expert at Star Wars. Introduce yourself and see if I have any questions."
 ]);
 
-$swml->set_aipost_prompt([
-    'text' => 'Summarize the conversation'
-]);
+$swml->set_aipost_prompt(['text' => 'Please summarize the conversation.']);
 
-$swml->add_aihints('jokes', 'weather', 'time');
+$swml->add_aihints("foo", "bar");
 
-$swml->set_aipost_prompt_url([
-    'post_prompt_url' => getenv('post_prompt_url')
-]);
+$swml->add_aiswaigdefaults(['web_hook_url' => '$ENV{webHookURL}']);
 
-$swml->add_aiswaig_defaults(['web_hook_url' => getenv('web_hook_url')]);
-
-$swml->add_aiswaig_function([
+$swml->add_aiswaigfunction([
     'function' => 'get_weather',
-    'purpose' => 'To determine what the current weather is in a provided location.',
-    'argument' => 'The location or name of the city to get the weather from.'
+    'purpose' => "To determine what the current weather is in a provided location.",
+    'argument' => "The location or name of the city to get the weather from."
 ]);
 
-$swml->add_aiswaig_function([
+$swml->add_aiswaigfunction([
     'function' => 'get_world_time',
-    'purpose' => 'To determine what the current time is in a provided location.',
-    'argument' => 'The location or name of the city to get the time from.'
+    'purpose' => "To determine what the current time is in a provided location.",
+    'argument' => "The location or name of the city to get the time from."
 ]);
 
-$swml->add_aiapplication('main');
+$swml->add_aiinclude([
+    'url' => 'https:/$ENV{WEB_AUTH_USER}:$ENV{WEB_AUTH_PASSWORD}@swaig-server.example.com/',
+    'functions' => 'transfer',
+    'meta_data_token' => '6a524622-c6aa-4311-aad7-ebf4d3e06879',
+    'meta_data' => [
+        'table' => [
+            'brian' => '$ENV{BRIAN_CELL}',
+            'support' => 'sip:support@pbx.example.com;transport=tcp',
+            'sales' => 'sip:sales@pbx.example.com;transport=tcp',
+            'carrier' => 'sip:carrier@pbx.example.com;transport=tcp'
+        ],
+        'config' => [
+            'response' => "call transferred, the call has ended.",
+            'message' => "Please stand by while I connect your call.",
+            'error' => "I'm sorry, I was unable to transfer your call.",
+            'hangup' => "true"
+        ]
+    ]
+]);
 
-header('Content-Type: application/json');
-echo $swml->render_json();
+$swml->add_aiinclude([
+    'url' => 'https:/$ENV{WEB_AUTH_USER}:$ENV{WEB_AUTH_PASSWORD}@swaig-server.example.com/',
+    'functions' => 'something_else',
+    'meta_data_token' => '6a524622-c6aa-4311-aad7-ebf4d3e06879',
+    'meta_data' => [
+        'table' => [
+            'brian' => '$ENV{BRIAN_CELL}',
+            'support' => 'sip:support@pbx.example.com;transport=tcp',
+            'sales' => 'sip:sales@pbx.example.com;transport=tcp',
+            'carrier' => 'sip:carrier@pbx.example.com;transport=tcp'
+        ],
+        'config' => [
+            'response' => "call transferred, the call has ended.",
+            'message' => "Please stand by while I connect your call.",
+            'error' => "I'm sorry, I was unable to transfer your call.",
+            'hangup' => "true"
+        ]
+    ]
+]);
 
+$swml->add_ainativefunction('check_time');
+$swml->add_ainativefunction('wait_seconds');
 
+$swml->add_aiapplication("main");
+
+$swml->add_application("test", "play", [
+    'urls' => ['https://github.com/freeswitch/freeswitch-sounds/raw/master/fr/ca/june/voicemail/48000/vm-goodbye.wav']
+]);
+
+$swml->add_application("test", "hangup", "NORMAL_CLEARING");
+
+$swml->add_application("blah", "play", [
+    'urls' => ['https://github.com/freeswitch/freeswitch-sounds/raw/master/en/us/callie/ivr/48000/ivr-welcome_to_freeswitch.wav']
+]);
+
+$swml->add_application("blah", "transfer", "test");
+
+$json_output = $swml->render_json();
+echo "JSON output:\n";
+echo $json_output . "\n\n";
+
+$yaml_output = $swml->render_yaml();
+echo "YAML output:\n";
+echo $yaml_output;
+
+?>
